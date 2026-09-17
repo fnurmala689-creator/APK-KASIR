b5753c586e7e9542606603bf68c691d38629fbbb
fnurmala689-creator <fnurmala689@gmail.com>
2026-09-17 21:58:53 +0700

    Update keu.py

--- keu.py
+++ keu.py
@@ -14,7 +14,7 @@ DEFAULT_CSV_URL = ""  # Masukkan link CSV publish to web Google Spreadsheet Anda
 url_spreadsheet = st.sidebar.text_input(
     "🔗 Link CSV Google Spreadsheet", 
     value=DEFAULT_CSV_URL,
-    placeholder="https://docs.google.com/spreadsheets/d/e/2PACX-1vRIw6LgDSUn_lDlosWSAGQra0bR597E_Av6OYoo9uRpVr1P9ROMMgSaS_OSjp1Jj3Sp5GBRV01lIh0k/pubhtml"
+    placeholder="https://docs.google.com/spreadsheets/d/e/2PACX-1vRIw6LgDSUn_lDlosWSAGQra0bR597E_Av6OYoo9uRpVr1P9ROMMgSaS_OSjp1Jj3Sp5GBRV01lIh0k/pub?gid=0&single=true&output=csv"
 )
 
 # Inisialisasi Database Produk Default (Jika belum connect spreadsheet)
