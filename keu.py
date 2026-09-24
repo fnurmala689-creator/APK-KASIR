6501c71729ca4a8c5e3f16c9043cdc77a58df8f0
fnurmala689-creator <fnurmala689@gmail.com>
2026-09-24 07:54:38 +0700

    Update keu.py

--- keu.py
+++ keu.py
@@ -697,11 +697,7 @@ else:
 
         for idx, item in enumerate(st.session_state.keranjang):
             row_c0, row_c0_cam, row_c1, row_c2, row_c3, row_c4 = st.columns([1.3, 0.5, 3, 2, 2, 1])
-
-
-
-        
-           with row_c0:
+            with row_c0:
                 val_bc = item.get("Input_Barcode", item.get("Barcode", ""))
 
                 components.html(f"""
@@ -735,7 +731,6 @@ else:
                 """, height=45)
 
                 
-
             with row_c0_cam:
                 st.markdown("<div style='margin-top: 2px;'>", unsafe_allow_html=True)
                 if st.button("📷", key=f"btn_cam_{idx}", help="Scan Kamera"):
